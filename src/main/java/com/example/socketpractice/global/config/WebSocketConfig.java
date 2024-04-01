899357eb4eae4e81a613b5544d970f489efcfe41
lsc713 <lsc7134.naver.com>
2024-04-01 12:39:45 +0900

    chore init login add login + security

--- src/main/java/com/example/socketpractice/global/config/WebSocketConfig.java
+++ src/main/java/com/example/socketpractice/global/config/WebSocketConfig.java
@@ -9,18 +9,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-//        registry.enableSimpleBroker("/sub");
-        registry.setApplicationDestinationPrefixes("/pub")
-                .enableStompBrokerRelay("/topic")
-                .setRelayHost("localhost")
-                .setVirtualHost("/")
-                .setRelayPort(61613)
-                .setClientLogin("guest")
-                .setClientPasscode("guest");
 
-    }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
