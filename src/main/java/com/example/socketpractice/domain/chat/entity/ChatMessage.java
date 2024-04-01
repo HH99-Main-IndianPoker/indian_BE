bd77d1ab0f66429662a45be9f9359d3e66c8197b
kjs4231 <kjs4231@naver.com>
2024-04-01 20:38:04 +0900

    feat 채팅시작중

--- src/main/java/com/example/socketpractice/domain/chat/entity/ChatMessage.java
+++ src/main/java/com/example/socketpractice/domain/chat/entity/ChatMessage.java
@@ -7,42 +7,29 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
-
-@Entity
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+
 public class ChatMessage {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @ManyToOne
-    private ChatRoom chatRoom;
-    @ManyToOne
-    private User user;
-    private String message;
-    private MessageType type;
+    private String content;
     private String sender;
-    private String roomId;
 
-    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
-    private LocalDateTime regData;
-
-
-    @Builder
-    public ChatMessage(String chatRoom, String user, String message) {
-        this.roomId = chatRoom;
-        this.sender = user;
-        this.message = message;
-    }
-
-    public void enterMessage(String message) {
-        this.message = message;
-        this.regData = LocalDateTime.now();
-    }
+    private MessageType type;
 
     public enum MessageType {
-        ENTER, TALK
+        CHAT,
+        JOIN,
+        LEAVE
+    }
+
+    public void setType(MessageType type) {
+        this.type = type;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
     }
 }
