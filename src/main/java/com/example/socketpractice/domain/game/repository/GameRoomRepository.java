0a890c1fc0666c5a712b2a2584bf0a2304bd169e
weiß Yi <78130010+rosa-eau@users.noreply.github.com>
2024-04-02 18:05:48 +0900

    Refactor : 패키지 rename

--- src/main/java/com/example/socketpractice/domain/game/repository/GameRoomRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.socketpractice.domain.game.repository;
-
-import com.example.socketpractice.domain.game.entity.GameRoom;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GameRoomRepository extends JpaRepository<GameRoom, Long> {
-}
